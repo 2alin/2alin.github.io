bc061463b309d2dcf1a82138f61d3c76e5995c4f
2alin <adilsonsdev@gmail.com>
2018-09-29 00:29:08 -0500

    add style to nav bars

---
+++ learn/fcc/product-page/main.js
@@ -0,0 +1,14 @@
+var menuButton = document.querySelector("#menu-section a");
+var companySection = document.querySelector("#company-section");
+
+
+menuButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if(menuButton.classList.contains("opened")) {
+    menuButton.classList.remove("opened");
+    companySection.style.display = "none";
+  }  else {
+    menuButton.classList.add("opened");
+    companySection.style.display = "flex";
+  }
+});
